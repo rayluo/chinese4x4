0ad35d759d64f9a9f8e21c3abafa6ccdebf161d4
Yingying Luo <yingyingbluo@gmail.com>
2022-08-28 13:30:15 -0700

    Added shuffling and answer checking functions

--- website/main.py
+++ website/main.py
@@ -1,17 +1,28 @@
 from browser import document, bind, html
+from random import shuffle
 # Note: If you could avoid unnecessary import, your script will save several seconds loading time
 # For example, use `"message".title()` instead of `import string; string.capwords("message")`
 
-#@bind("#say", "click")
-#def echo(event):
-#    message = document["message"].value
-#    document["output"].text = message.title()
+initial_cards = "一知半解一心一意一丘之貉一目了然"
+
+@bind("#check", "click")
+def check(event):
+    wrong_count = 0
+    for i, cell in enumerate(document.select("#table td")):
+        if initial_cards[i] != cell.text:
+            wrong_count = wrong_count + 1
+
+    document["result"].text = (
+        str(wrong_count) + " characters are in the wrong place."
+        ) if wrong_count != 0 else "Correct!"
 
 def create_cards(characters):
     for i, c in enumerate(characters):
         document["cards"].attach(html.DIV(c, id="char{}".format(i)))
 
-create_cards("一知半解一心一意一丘之貉一目了然")
+cards_list = list(initial_cards)
+shuffle(cards_list)
+create_cards(cards_list)
 
 def mouseover(event):
     event.target.style.cursor = "pointer"
